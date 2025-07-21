8ce864e5255f95ad8f28d7b18fb2b7cb513e8c4b
Nitesh Kr. Singh <136103300+SmakcT@users.noreply.github.com>
2025-07-21 18:58:09 +0000

    updated

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,10 @@
-console.log("Nitesh")
+const accountId = 12435
+let accountName = "Nitesh"
+var accountCity = "Kathmandu"
+accountType = "Demo"
+let accountTime;
+
+
+// var is not preffered as it has block scope problem or functional scope problem
+
+console.table([accountId, accountName, accountCity, accountType, accountTime])
